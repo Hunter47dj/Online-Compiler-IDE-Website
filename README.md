67ac0266d45b523f1efb33c289476cea444f53b1
Dharmendra Choudhary <ddlu268@gmail.com>
2024-01-29 00:13:59 +0530

    just an ordinary compiler

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Online-Compiler-Website
- Just an ordinary online compiler ide 
+# Online Code Compiler IDE 🚀
+
+Welcome to our Online Code Compiler IDE – a dynamic coding playground built with simplicity and functionality in mind! 🌟
+
+## Features 🎨
+
+- **Theme Selection:** Personalize your coding experience with a range of dynamic themes, from the elegance of Dracula to classic light themes.
+
+- **Visual Studio Vibes:** Enjoy familiar functionalities inspired by Visual Studio, including autocomplete brackets and syntax highlighting.
+
+- **Multi-Language Support:** Code fluently in three powerful languages – C++, Java, and Python. See your creations come to life instantly!
+
+- **Download Code:** Save your code masterpiece locally for future reference or offline editing.
+
+## Getting Started 🚀
+
+1. Clone the repository: `git clone https://github.com/Hunter47dj/online-compiler-website.git`
+2. Navigate to the project directory: `cd online-compiler-website`
+3. Install the required dependencies: `npm install`
+4. Start the API server by running the following command: `node api.js`
+5. Open your browser and go to [http://localhost:8000](http://localhost:8000).
+
+Now, you can choose your preferred theme and language and start coding to witness the magic unfold!
+
+## Contributions and Issues 🤝
+
+We welcome contributions and feedback! If you find a bug or have a feature request, please open an issue. Feel free to submit a pull request with improvements. Let's make this IDE even better together! 🌈
+
+Happy coding! 🚀👩‍💻👨‍💻
---
+++ online compiler website/api.js
@@ -0,0 +1,111 @@
+const express = require("express")
+const app= express()
+const bodyP= require("body-parser")
+const compiler= require("compilex")
+var hackerRank = require('machinepack-hackerrank');
+const options= {stats: true}
+compiler.init(options)
+
+app.use(bodyP.json())
+app.use("/codemirror-5.65.16",express.static("./codemirror-5.65.16"))
+app.get("/",function(req,res){
+    compiler.flush(function(){
+        console.log("deleted")
+    })
+    res.sendFile("C:/Users/DeLL/Desktop/PROJECTS/online compiler website/index.html")
+})
+
+app.post("/compile", function(req,res){
+    var code=req.body.code
+    var input=req.body.input
+    var lang=req.body.lang
+
+    try {
+        if(lang=="Cpp"){
+            if(!input){
+                var envData = { OS : "windows" , cmd : "g++", options: { timeout: 10000}};       // from compilex 
+                compiler.compileCPP(envData , code , function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }
+
+                });
+
+            }
+            else {
+                                //from compilex
+                var envData = { OS : "windows" , cmd : "g++", options: { timeout: 10000}}; 
+                compiler.compileCPPWithInput(envData , code , input , function (data) {
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }
+                });
+            }
+        } 
+        else if(lang=="Java"){
+            if(!input){
+                                // from comilex
+                var envData = { OS : "windows"}; 
+                compiler.compileJava( envData , code , function(data){
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }
+                });    
+            }
+            else {
+                                //  from comilex
+                var envData = { OS : "windows"}; 
+                compiler.compileJavaWithInput( envData , code , input ,  function(data){
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }
+                });
+            }
+        }
+        else if(lang=="Python"){
+            if(!input){
+                                 //from comilex
+                var envData = { OS : "windows"}; 
+                compiler.compilePython( envData , code , function(data){
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }
+                });    
+            }
+            else{
+                            //from comilex
+                var envData = { OS : "windows"}; 
+                compiler.compilePythonWithInput( envData , code , input ,  function(data){
+                    if (data.output) {
+                        res.send(data);
+                        
+                    } else {
+                        res.send({output:"error"})
+                    }        
+                });
+            }
+        } else if(lang=="Choose"){
+            res.send({output:"please select language"})
+        }
+        
+    } catch (e) {
+        console.log("error")
+    }
+}) 
+
+app.listen(8000)
